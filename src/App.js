135d6d61c4a003027c20671a1ecd4410dbfc6836
avshetty1980 <avshetty1980@gmail.com>
2020-10-08 14:38:38 +0100

    Starting blog App with Nav and static pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./App.css";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesList from "./pages/ArticlesList";
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article/:name" component={ArticlePage} />
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutPage.js
@@ -0,0 +1,65 @@
+import React from "react";
+
+const AboutPage = () => (
+  <>
+    <h1>About Me!</h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut velit a
+      risus mollis dictum vitae nec diam. Praesent in magna egestas, rhoncus
+      purus vitae, porta dolor. Phasellus orci nisl, auctor et pharetra sed,
+      sagittis at sapien. In hendrerit consectetur nulla vel semper. Cras
+      dapibus non dui a suscipit. Pellentesque eget hendrerit orci. Nunc ac nibh
+      eget libero egestas blandit. Suspendisse vitae lacinia odio, ac fermentum
+      turpis. Morbi posuere porta ante, lacinia aliquet velit ornare at . Donec
+      quis accumsan risus, at fermentum magna. Fusce id libero leo.{" "}
+    </p>
+
+    <p>
+      Suspendisse condimentum nisl purus, non ultrices nulla ornare et. Mauris
+      elementum et diam ut faucibus. Suspendisse aliquet rhoncus tellus. Mauris
+      varius eget ipsum at gravida. Donec a metus sed felis ultrices porta vitae
+      quis velit. Vestibulum eget cursus metus. Donec at convallis enim. Fusce
+      scelerisque, ipsum quis ultrices lacinia, mi lorem sollicitudin elit, id
+      rhoncus purus tellus in mauris. Suspendisse nec dui ut nunc imperdiet
+      vehicula. Aliquam leo urna, ultrices nec mauris eu, sagittis interdum
+      quam. Fusce consequat dapibus congue.
+    </p>
+
+    <p>
+      Nulla a efficitur metus. In sit amet dolor at nulla hendrerit imperdiet
+      sed nec urna. Duis finibus ullamcorper magna a tempor. Praesent facilisis
+      scelerisque sem, eu pharetra eros congue sit amet. Maecenas sollicitudin
+      lectus at arcu mollis blandit. Pellentesque varius odio eu lacus porta,
+      vitae semper metus ultrices. Curabitur dapibus feugiat mauris non
+      bibendum.
+    </p>
+
+    <p>
+      Phasellus sed metus ac neque pulvinar malesuada et a odio. Morbi dui
+      felis, accumsan ac bibendum sed, malesuada vel sapien. Curabitur sed
+      iaculis risus, non facilisis lorem. Sed faucibus sit amet felis a posuere.
+      Sed ullamcorper elit vel lacus vehicula, ut malesuada lectus dapibus. Sed
+      sagittis faucibus consequat. Duis tempor eget urna eget porta. Quisque vel
+      felis eget nisl malesuada luctus. Sed a maximus ligula. Aliquam at eros
+      magna. Proin vitae augue eget dui finibus rutrum nec sed elit. Donec non
+      sollicitudin libero, in dictum orci. Fusce id odio sollicitudin, fermentum
+      purus non, malesuada urna. Nulla tincidunt diam sed elit vulputate, sed
+      efficitur magna rutrum. Aliquam pretium, sem et maximus luctus, metus elit
+      efficitur tellus, non hendrerit eros nunc posuere dolor.
+    </p>
+
+    <p>
+      Integer sed orci ligula. Duis arcu nulla, interdum malesuada accumsan sit
+      amet, ullamcorper quis sapien. Pellentesque eget est congue, hendrerit
+      libero quis, euismod turpis. Aenean condimentum quam et eros pulvinar, a
+      imperdiet metus posuere. Nam nunc sapien, molestie vel quam ac, faucibus
+      ultrices elit. Fusce id lectus lobortis, semper sapien sed, pretium leo.
+      Integer cursus finibus molestie. Pellentesque ac finibus arcu, sed pretium
+      sapien. Sed vehicula nisi in sagittis viverra. Nulla elit enim, fringilla
+      a nibh vel, euismod placerat turpis. Aliquam erat volutpat. Aliquam
+      sagittis pretium volutpat.
+    </p>
+  </>
+);
+
+export default AboutPage;
